78633389e3a2fc7efe54675a41e1632f78bc7d07
lmacinnes <lmacinnes@etsy.com>
2017-02-01 00:32:56 +0000

    added symlink script

---
+++ links.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+ln -sv ${HOME}/dotfiles/vimrc ${HOME}/.vimrc
+ln -sv ${HOME}/dotfiles/zshrc ${HOME}/.zshrc
+ln -sv ${HOME}/dotfiles/tmux.conf ${HOME}/.tmux.conf
+ln -sv ${HOME}/dotfiles/vim ${HOME}/.vim
+ln -sv ${HOME}/dotfiles/zgen ${HOME}/.zgen
